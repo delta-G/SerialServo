53087eb59f24d1829553fb7e6c6fc22c9ae50202
delta-G <disc047dave@gmail.com>
2016-06-19 22:56:19 -0500

    First Commit

---
+++ SerialServo.cpp
@@ -0,0 +1,67 @@
+
+#include "SerialServo.h"
+
+
+int pos = 0;
+int servo = 0;
+boolean reading = false;
+boolean gotServo = false;
+
+Servo servos[6];
+
+void setup(){
+
+  Serial.begin(115200);
+
+  servos[0].write(90);
+  servos[1].write(65);
+  servos[2].write(65);
+  servos[3].write(170);
+  servos[4].write(90);
+  servos[5].write(120);
+
+  for(int i = 0; i<6; i++){
+    servos[i].attach(i + 4);
+  }
+
+  delay(500);
+
+}
+
+void loop(){
+
+  if(Serial.available()){
+    char c = Serial.read();
+
+    if(c == '<'){
+      pos = 0;
+      servo = 0;
+      reading = true;
+      gotServo = false;
+    }
+
+    else if(c == '>' && reading == true){
+      reading = false;
+      if(pos >= 0 && pos <= 180 && servo < 6){
+
+        Serial.print("Running servo ");
+        Serial.print(servo);
+        Serial.print(" to position :");
+        Serial.println(pos);
+        servos[servo].write(pos);
+      }
+    }
+
+    else if(c == ',' && !gotServo){
+      servo = pos;
+      pos = 0;
+      gotServo = true;
+    }
+
+    else if(c >= '0' && c <= '9' && reading == true){
+      pos *= 10;
+      pos += (c - '0');
+    }
+  }
+}
+
---
+++ SerialServo.h
@@ -0,0 +1,19 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _SerialServo_H_
+#define _SerialServo_H_
+#include "Arduino.h"
+
+#include <Servo.h>
+
+void setup();
+void loop();
+
+
+
+
+#endif /* _SerialServo_H_ */
